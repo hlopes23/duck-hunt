e08454d136b7a8c399c6a899d183042bd7473fb4
admin.mindera <raquel.lopes@school.mindera.com>
2025-03-24 14:14:13 +0000

    changed code

--- script/sprite.js
+++ script/sprite.js
@@ -1,55 +1,106 @@
+const positionAndSizePurpleHorizontalDuck = [{ x: 0, y: -121, w: 34, h: 24 }, { x: -40, y: -123, w: 34, h: 24 }, { x: -81, y: -119, w: 34, h: 24 }];
+const purpleDuck = document.getElementById("purpleHorizontalDuck");
+let horizontalMoveInterval;
 let frameIndex = 0;
 let positionX = 0;
-const frameWidth = 34;
-const totalFrames = 3;
-const duck = document.getElementById("image");
-let windowWidth = window.innerWidth;
-let movingInterval;
-
-function moveHorizontal() {
-    movingInterval = setInterval(() => {
-    frameIndex = (frameIndex + 1) % totalFrames;
-    let posX = -frameIndex * frameWidth;
-    duck.style.backgroundPosition = `${posX}px -121px`;
-
-    positionX += 5;
-    duck.style.transform = `translateX(${positionX}px)`;
-
-    if (positionX >= windowWidth) {
-      clearInterval(interval);
-      positionX = windowWidth;
-
-      let reverseInterval = setInterval(() => {
-        frameIndex = (frameIndex + 1) % totalFrames;
-        let posX = -frameIndex * frameWidth;
-        duck.style.backgroundPosition = `${posX}px -121px`;
-
-        positionX -= 5;
-        duck.style.transform = `translateX(${positionX}px) scale(-1,1)`;
-      }, 250);
-    }
-  }, 150);
-}
-moveHorizontal();
-
-
 let positionY = 0;
-let fallingDuckPositionX = 0;
+let width = 0;
+let height = 0;
+let posX = 0;
+
+function purpleDuckHorizontalMove() {
+    horizontalMoveInterval = setInterval(() => {
+        if (frameIndex < positionAndSizePurpleHorizontalDuck.length) {
+            positionX = positionAndSizePurpleHorizontalDuck[frameIndex].x;
+            positionY = positionAndSizePurpleHorizontalDuck[frameIndex].y;
+            width = positionAndSizePurpleHorizontalDuck[frameIndex].w;
+            height = positionAndSizePurpleHorizontalDuck[frameIndex].h;
+            purpleDuck.style.backgroundPosition = `${positionX}px ${positionY}px`;
+            purpleDuck.style.width = `${width}px`;
+            purpleDuck.style.height = `${height}px`;
+            frameIndex++
+
+            posX += 10;
+            purpleDuck.style.transform = `translateX(${posX}px)`;
+        }
+        else {
+            frameIndex = 0;
+        }
+
+        if(positionX >= window.innerWidth) {
+            clearInterval(horizontalMoveInterval);
+            purpleDuck.style.visibility = "hidden";
+        }
+    }, 100);
+}
+purpleDuckHorizontalMove();
+
+let positionAndSizePurpleFallingDuck = [{x: -1, y: -238, w:31, h:29}, {x: -48, y: -237, w:31, h:29}];
+let fallingInterval;
+let posY = 0;
 
 function fallingDuck() {
-    let interval = setInterval(() => {
-        clearInterval(movingInterval);
+    clearInterval(horizontalMoveInterval);
 
-        duck.style.backgroundPosition = `-1px -238px`;
-        duck.style.backgroundPosition = `-48px -237px`;
+    fallingInterval = setInterval(() => {
+        if(frameIndex < positionAndSizePurpleFallingDuck.length) {
+            positionX = positionAndSizePurpleFallingDuck[frameIndex].x;
+            positionY = positionAndSizePurpleFallingDuck[frameIndex].y;
+            width = positionAndSizePurpleFallingDuck[frameIndex].w;
+            height = positionAndSizePurpleFallingDuck[frameIndex].h;
+            purpleDuck.style.backgroundPosition = `${positionX}px ${positionY}px`;
+            purpleDuck.style.width = `${width}px`;
+            purpleDuck.style.height = `${height}px`;
+            frameIndex++
 
-        positionY += 5;
-        duck.style.transform = `translate(${positionX}px, ${positionY}px)`;
-
-        if(positionY === window.innerHeight) {
-            clearInterval(interval);
-            duck.style.visibility = "hidden";
+            posY += 5;
+            purpleDuck.style.transform = `translate(${posX}px, ${posY}px)`;
+        } else {
+            frameIndex = 1;
         }
-}, 40)}
 
-duck.addEventListener("click", fallingDuck);
+        if(positionY >= window.innerHeight) {
+            clearInterval(fallingInterval);
+            purpleDuck.style.visibility = "hidden";
+        }
+    }, 20)
+}
+
+purpleDuck.addEventListener("click", fallingDuck);
+
+
+Array.prototype.sumNumbersOrStrings = () => {
+    for(let i = 0; i<Array.length; i++) {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
