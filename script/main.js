2cd51a5e2cf2b2267bea91a052d7b36f1367a777
mindera teste <henrique.silva.lopes@outlook.pt>
2025-03-26 15:48:52 +0000

    dog jumping over the herbs. animation to be perfectioned

--- script/main.js
+++ script/main.js
@@ -1,34 +1,5 @@
 
-//      { x: -245, y: -1, w: 55, h: 43 }, smell
-
-let positionsAndSize = [
-  { x: -5, y: -1, w: 55, h: 43 },
-  { x: -66, y: -1, w: 55, h: 43 },
-  { x: -126, y: -1, w: 55, h: 43 },
-  { x: -184, y: -1, w: 55, h: 43 },
-  { x: -245, y: -1, w: 55, h: 43 },
-  { x: -5, y: -59, w: 55, h: 50 },
-  { x: -74, y: 60, w: 40, h: 46 },
-  { x: -135, y: 67, w: 32, h: 32 },
-];
-
-
-const dog = document.getElementById("dog");
-const playground = document.getElementById("passeio");
-let interval;
-let frameIndex = 0;
-let positionX = 0;
-let positionY = 0;
-let width = 0;
-let height = 0;
-let playgroundWidth = playground.offsetWidth;
-let dogWidth = dog.offsetWidth;
-let xTranslate = 0;
-let stopMiddle = false;
-
-
-
-function moveX() {
+function move() {
 
 
   interval = setInterval(() => {
@@ -36,10 +7,14 @@ function moveX() {
     let end = playgroundWidth - dogWidth
 
     if (xTranslate >= end) {
-
       clearInterval(interval);
       smell();
-      return;
+      setTimeout (() => { 
+        dog.style.backgroundPosition = `${-5}px ${-59}px`;
+        dog.style.width = `${55}px`;
+        dog.style.height = `${50}px`;
+        jump();
+       }, 1500);
     }
 
 
@@ -47,12 +22,12 @@ function moveX() {
       stopMiddle = true;
       clearInterval(interval);
       smell();
-      setTimeout (moveX, 1000);
+      setTimeout (move, 1000);
       return;
     }
 
 
-    if (frameIndex < 4){
+    if (frameIndex < positionsAndSize.length-2){
       positionX = positionsAndSize[frameIndex].x;
       positionY = positionsAndSize[frameIndex].y;
       width = positionsAndSize[frameIndex].w;
@@ -74,52 +49,68 @@ function moveX() {
 function smell() {
 
     let smellCount = 0;
-    let i = 3; 
+    frameIndex = 3; 
 
     interval = setInterval( () => {
-        positionX = positionsAndSize[i].x;
-        positionY = positionsAndSize[i].y;
-        width = positionsAndSize[i].w;
-      height = positionsAndSize[i].h;
+      positionX = positionsAndSize[frameIndex].x;
+      positionY = positionsAndSize[frameIndex].y;
+      width = positionsAndSize[frameIndex].w;
+      height = positionsAndSize[frameIndex].h;
       dog.style.backgroundPosition = `${positionX}px ${positionY}px`;
       dog.style.width = `${width}px`;
       dog.style.height = `${height}px`;
       dog.style.transform = `translateX(${xTranslate}px) scale(3.5)`;
       
-    
-      i = (i === 3) ? 4 : 3;
+      frameIndex = (frameIndex === 3) ? 4 : 3;
       smellCount++;
 
       if(smellCount > 6){
       clearInterval(interval);
-      return;
     }
 }, 140);
 }
 
 
-function jump(){
-
-    let i = 5;
-    interval = setInterval( () => {
-        positionX = positionsAndSize[i].x;
-        positionY = positionsAndSize[i].y;
-        width = positionsAndSize[i].w;
-      height = positionsAndSize[i].h;
-      dog.style.backgroundPosition = `${positionX}px ${positionY}px`;
-      dog.style.width = `${width}px`;
-      dog.style.height = `${height}px`;
-      xTranslate += 15;
-      dog.style.transform = `translateX(${xTranslate}px) scale(3.5)`;
-
-      i = (i === 5) ? 6 : (i === 6) ? 7 : 5;
-}, 70);
+function jump() {
+dog.classList.add('jump');
 }
 
 
-dog.addEventListener("click", () => {
-  clearInterval(interval);
-});
+
+// -- variables -- 
 
 
-moveX();
+
+let positionsAndSize = [
+  { x: -5, y: -1, w: 55, h: 43 },
+  { x: -66, y: -1, w: 55, h: 43 },
+  { x: -126, y: -1, w: 55, h: 43 },
+  { x: -184, y: -1, w: 55, h: 43 },
+  { x: -245, y: -1, w: 55, h: 43 }, // smell
+  { x: -5, y: -59, w: 55, h: 50 } 
+];
+
+
+
+let dog = document.createElement("div");
+let playground = document.getElementById("path");
+dog.setAttribute("id", "dog");
+playground.appendChild(dog);
+let interval;
+let frameIndex = 0;
+let positionX = 0;
+let positionY = 0;
+let width = 0;
+let height = 0;
+let playgroundWidth = playground.offsetWidth;
+let dogWidth = dog.offsetWidth;
+let xTranslate = 0;
+let yTranslate = 0;
+let zTranslate = 0;
+let stopMiddle = false;
+
+
+
+
+
+move();
