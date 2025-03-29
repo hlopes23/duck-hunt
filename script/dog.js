808853bac976d3a482466c471f1d9b48a2271c79
admin.mindera <raquel.lopes@school.mindera.com>
2025-03-29 12:31:06 +0000

    added sounds

---
+++ script/dog.js
@@ -0,0 +1,169 @@
+import { posXDiagonalDuck } from "./duck.js";
+import { posYDiagonalDuck } from "./duck.js";
+
+export function moveDog() {
+
+
+  interval = setInterval(() => {
+    let middle = (playgroundWidth - dogWidth) / 2;
+    let end = playgroundWidth - dogWidth
+
+    if (xTranslate >= end) {
+      clearInterval(interval);
+      smell();   
+      setTimeout(jump,1000);
+      return;
+    }
+
+
+    if (xTranslate >= middle && !stopMiddle) {
+      stopMiddle = true;
+      clearInterval(interval);
+      smell();
+      setTimeout (moveDog, 1000);
+      return;
+    }
+
+
+    if (frameIndex < positionsAndSize.length-2){
+      positionX = positionsAndSize[frameIndex].x;
+      positionY = positionsAndSize[frameIndex].y;
+      width = positionsAndSize[frameIndex].w;
+      height = positionsAndSize[frameIndex].h;
+      dog.style.backgroundPosition = `${positionX}px ${positionY}px`;
+      dog.style.width = `${width}px`;
+      dog.style.height = `${height}px`;
+      xTranslate += 15;
+      dog.style.transform = `translateX(${xTranslate}px) scale(3.5)`;
+      frameIndex = (frameIndex + 1) % positionsAndSize.length;
+    } else {
+      frameIndex = 0;
+    }
+  }, 70);
+}
+
+
+function smell() {
+
+    let smellCount = 0;
+    frameIndex = 3; 
+
+    interval = setInterval( () => {
+      positionX = positionsAndSize[frameIndex].x;
+      positionY = positionsAndSize[frameIndex].y;
+      width = positionsAndSize[frameIndex].w;
+      height = positionsAndSize[frameIndex].h;
+      dog.style.backgroundPosition = `${positionX}px ${positionY}px`;
+      dog.style.width = `${width}px`;
+      dog.style.height = `${height}px`;
+      dog.style.transform = `translateX(${xTranslate}px) scale(3.5)`;
+      
+      frameIndex = (frameIndex === 3) ? 4 : 3;
+      smellCount++;
+
+      if(smellCount > 6){
+      clearInterval(interval);
+    }
+}, 140);
+}
+
+
+function jump() {
+
+  dog.classList.add('transition');
+
+  frameIndex = 0;
+
+  interval = setInterval( () => {
+    positionX = jumpPositions[frameIndex].x;
+    positionY = jumpPositions[frameIndex].y;
+    width = jumpPositions[frameIndex].w;
+    height = jumpPositions[frameIndex].h;
+
+    switch (frameIndex) {
+
+      case 0:
+      dog.style.backgroundPosition = `${positionX}px ${positionY}px`;
+      dog.style.width = `${width}px`;
+      dog.style.height = `${height}px`;
+      frameIndex++;
+      break;
+
+      case 1:
+      dog.style.backgroundPosition = `${positionX}px ${positionY}px`;
+      dog.style.width = `${width}px`;
+      dog.style.height = `${height}px`;
+      dog.style.transform = `translate3d(900px, ${-herbsHeight}px, -150px) scale(3.5)`;
+      frameIndex++;
+      break;
+
+      case 2:
+        dog.style.backgroundPosition = `${positionX}px ${positionY}px`;
+        dog.style.width = `${width}px`;
+        dog.style.height = `${height}px`;
+        dog.style.transform = `translate3d(1010px, 0px, -0px) scale(3.5)`;
+        dog.style.zIndex = 0;
+        clearInterval(interval);
+        break;
+    }
+  }, 300);
+}
+
+
+export function catchDuck() {
+	
+  if (posYDiagonalDuck >= footerHeight){
+
+  xTranslate = posXDiagonalDuck || 0;
+  dog.style.right = `${xTranslate}px`; 
+
+  interval = setInterval( () => {
+  dog.classList.add('dogWithDuck');
+  dog.style.backgroundPosition = `${catchDuckPositions[0].x}px ${catchDuckPositions[0].y}px`;
+  dog.style.width = `${catchDuckPositions[0].w}px`;
+  dog.style.height = `${catchDuckPositions[0].h}px`;
+  
+}, 300);
+}
+}
+
+
+// -- variables -- 
+
+let positionsAndSize = [
+  { x: -5, y: -1, w: 55, h: 43 },
+  { x: -66, y: -1, w: 55, h: 43 },
+  { x: -126, y: -1, w: 55, h: 43 },
+  { x: -184, y: -1, w: 55, h: 43 },
+  { x: -245, y: -1, w: 55, h: 43 }, // smell
+];
+
+let jumpPositions = [
+  { x: -5, y: -59, w: 55, h: 50 }, 
+  { x: -74, y: -60, w: 55, h: 50 }, 
+  { x: -135, y: -67, w: 55, h: 50 }
+];
+
+let catchDuckPositions = [
+  { x: -332, y: -3, w: 55, h: 50 }, //1 duck
+  { x: -319, y: -69, w: 55, h: 50 }, // 2 ducks
+]
+
+
+let herbs = document.getElementById('herbs');
+export let herbsHeight = herbs.offsetHeight;
+let dog = document.createElement("div");
+let playground = document.getElementById("path");
+dog.setAttribute("id", "dog");
+playground.appendChild(dog);
+let interval;
+export let footerHeight = document.querySelector('footer').offsetHeight;
+let frameIndex = 0;
+let positionX = 0;
+let positionY = 0;
+let width = 0;
+let height = 0;
+let playgroundWidth = playground.offsetWidth;
+let dogWidth = dog.offsetWidth;
+let xTranslate = 0;
+let stopMiddle = false;
---
+++ script/duck.js
@@ -0,0 +1,212 @@
+import { randomMoves } from "./game.js";
+import { catchDuck, herbsHeight } from "./dog.js";
+import { randomIndex } from "./game.js";
+
+
+export const positionAndSizeDiagonalDuck = [
+	{ x: -134, y: -157, w: 32, h: 31 },
+	{ x: -171, y: -158, w: 32, h: 31 },
+	{ x: -213, y: -157, w: 32, h: 31 },
+];
+
+export const positionAndSizeTurningDuck = [
+	{ x: -135, y: -197, w: 32, h: 31 },
+	{ x: -171, y: -197, w: 32, h: 31 },
+	{ x: -212, y: -198, w: 32, h: 31 },
+];
+
+export function createDuckElement() {
+	const duck = document.createElement("div");
+	duck.id = "duck";
+	duck.style.position = "absolute";
+	duck.style.background = 'url("../img/sprite.webp") no-repeat';
+	duck.style.backgroundPosition = "-134px -157px";
+	duck.style.width = "32px";
+	duck.style.height = "31px";
+	duck.style.transform = "scale(2.0) scaleX(-1)";
+	duck.style.transformOrigin = "bottom left";
+	document.body.appendChild(duck);
+	return duck;
+}
+
+
+function redDuck() {
+	const duckIcon = document.querySelectorAll(".icon");
+	for (let ducks of duckIcon) {
+		if (ducks.src !== "/img/redDuck.png") {
+			ducks.src = "/img/redDuck.png";
+			break;
+		}
+	}
+}
+
+function shots() {
+	numberOfShots--;
+	const bullet = document.querySelectorAll(".bullet");
+	for (let bullets of bullet) {
+		if (bullets.style.visibility != "hidden") {
+			bullets.style.visibility = "hidden";
+			break;
+		}
+	}
+
+	if (hitDuck == true && bullet.style.visibility == "hidden") {
+		numberOfShots = 3;
+		bullet.forEach(bullet => bullet.style.visibility = "visible");
+	}
+}
+
+function score() {
+	scoreNumber += 500;
+	let points = document.querySelector(".scoreElement");
+	points.style.fontSize = "24px";
+	points.style.fontWeight = "bold";
+	points.style.color = "white";
+	points.textContent = `${scoreNumber}`;
+}
+
+function shotSound() {
+	let music = document.getElementById("shotSound");
+	music.play();
+}
+
+function duckCaughtSound () {
+	let music = document.getElementById("duckCaught");
+	music.play();
+}
+
+function duckFallingSound () {
+	let music = document.getElementById("deadDuckFalling");
+	music.play();
+}
+
+function flappingSound () {
+	let music = document.getElementById("flappingSound");
+	music.play();
+}
+
+document.addEventListener("click", () => {
+	failedShots(),
+		shotSound()
+});
+
+function failedShots() {
+	shots();
+	if (numberOfShots <= 0) {
+		freezeGame();
+		const gameOver = document.createElement("div");
+		gameOver.textContent = "GAME OVER";
+		gameOver.style.color = "red";
+		gameOver.style.position = "absolute";
+		gameOver.style.top = "50%";
+		gameOver.style.left = "40%";
+		gameOver.style.fontSize = "50px"
+		document.body.appendChild(gameOver);
+	}
+}
+
+function freezeGame() {
+	document.removeEventListener("click", failedShots);
+	clearInterval(interval);
+
+	duck.style.animationPlayState = "paused";
+}
+
+
+
+export function moveDuck() {
+	const duck = document.getElementById("duck") || createDuckElement();
+	let currentMove = randomMoves[randomIndex];
+
+	flappingSound();
+
+
+	duck.addEventListener("click", () => {
+		hitDuck = true;
+		shotSound();
+		duckFallingSound(),
+		fallingDuck(),
+			redDuck(),
+			score()
+	}
+	);
+
+	interval = setInterval(() => {
+		if (frameIndex < currentMove.pos.length) {
+			currentFrame = currentMove.pos[frameIndex];
+			duck.style.backgroundPosition = `${currentFrame.x}px ${currentFrame.y}px`;
+			frameIndex++;
+
+			posXDiagonalDuck += currentMove.x * movementDirection;
+			posYDiagonalDuck += currentMove.y * movementDirection;
+
+			if (
+				posXDiagonalDuck <= 0 ||
+				posXDiagonalDuck >= window.innerWidth - 32 * 2.0 ||
+				posYDiagonalDuck <= 0 && posYDiagonalDuck >= herbsHeight ||
+				posYDiagonalDuck >= window.innerHeight - 31 * 2.0
+			) {
+				movementDirection *= -1;
+
+				duck.style.transform = `
+                    translate(${posXDiagonalDuck}px, ${posYDiagonalDuck}px) 
+                    scale(3.0) 
+                    scaleX(${movementDirection > 0 ? "-1" : "1"})
+                `;
+			} else {
+				duck.style.transform = `
+                    translate(${posXDiagonalDuck}px, ${posYDiagonalDuck}px) 
+                    scale(3.0) 
+                    scaleX(${movementDirection > 0 ? "-1" : "1"})
+                `;
+			}
+		} else {
+			frameIndex = 0;
+		}
+	}, 80);
+}
+
+export function fallingDuck() {
+	clearInterval(interval);
+	const duck = document.getElementById("duck") || createDuckElement();
+	let frameIndex = 0;
+	console.log("morreu");
+	let positionDuck = [
+		{ x: -131, y: -238 },
+		{ x: -178, y: -237 },
+	];
+
+	const fallingInterval = setInterval(() => {
+		if (frameIndex < positionDuck.length) {
+			const currentFrame = positionDuck[frameIndex];
+			duck.style.backgroundPosition = `${currentFrame.x}px ${currentFrame.y}px`;
+			frameIndex++;
+			posYDiagonalDuck += 15;
+			duck.style.zIndex = "0";
+			duck.style.transform = `
+                translate(${posXDiagonalDuck}px, ${posYDiagonalDuck}px)
+                scale(3.0)
+            `;
+		} else {
+			frameIndex = 1;
+		}
+
+		if (posYDiagonalDuck >= window.innerHeight) {
+			clearInterval(fallingInterval);
+			duck.style.visibility = "hidden";
+			catchDuck();
+			setTimeout(duckCaughtSound(), 1000);
+		}
+	}, 25);
+}
+
+
+let hitDuck = false;
+let currentFrame;
+let interval;
+let frameIndex = 0;
+export let posXDiagonalDuck = window.innerWidth - 210 * 2.0;
+export let posYDiagonalDuck = window.innerHeight - 150 * 2.0;
+let movementDirection = 1;
+let scoreNumber = 0;
+let numberOfShots = 3;
